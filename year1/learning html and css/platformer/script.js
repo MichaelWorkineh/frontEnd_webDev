2a06fd37ecf1aeb8f3f82b2a0caffa9c957c854e
Michael Workineh <micwor96@gmail.com>
2023-05-16 17:58:50 -0700

    a simple way to animate a character

--- year1/learning html and css/platformer/script.js
+++ year1/learning html and css/platformer/script.js
@@ -6,20 +6,23 @@ const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();//image class constructor
 playerImage.src = "resources/shadow_dog.png";
-x=0,fx=0;
-y=1;
+let frameX = 0;
+let frameY = 5;
+let gameFrame =0;
+const staggerframe = 4;
 const spritewidth = 575;
 const spriteheight = 523;
 
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
     //ctx.fillRect(100,50,100,100);
-    ctx.drawImage(playerImage,fx*spritewidth,y*spriteheight,spritewidth,spriteheight,0,0,spritewidth,spriteheight);
-    x++;
-    if(x%5==0) fx++;
-    if(fx==7) fx=0,y++;
-    if(y==3) y=0;
-     //y=0;
+    ctx.drawImage(playerImage,frameX*spritewidth,frameY*spriteheight,spritewidth,spriteheight,0,0,spritewidth,spriteheight);
+    if(gameFrame%staggerframe==0){
+        if(frameX<4) frameX++;
+        else frameX=0;
+    }
+    gameFrame++;
+    
     requestAnimationFrame(animate);
 };
 animate();
