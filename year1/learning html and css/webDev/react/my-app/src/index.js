3eca8db7bc135160ada540110687b8071617d322
Michael Workineh <micwor96@gmail.com>
2023-07-08 22:55:08 -0700

    finaly loaded the json file

--- year1/learning html and css/webDev/react/my-app/src/index.js
+++ year1/learning html and css/webDev/react/my-app/src/index.js
@@ -1,10 +1,23 @@
 import { type } from "@testing-library/user-event/dist/type";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Testing from "./Testing";
 
 function Login() {
+  const [peopleData, setPeopleData] = useState([null]);
+  useEffect(() => {
+    fetch("http://localhost:8000/users")
+      .then((res) => {
+        return res.json();
+      }) 
+      .then(data => {
+        console.log(data);
+        setPeopleData(data);
+      });
+  }, []);
+
+  //-------------------------------------------
   const registeredInfo = {
     name: "Mark Fishback",
     password: "blood shot eyes for my birthday",
@@ -42,8 +55,10 @@ function Login() {
           'sup
           <button onClick={logout}>logout</button>
         </div>
-      ) : (
+      ) : (<div>
+        {peopleData[5] && <h1> {peopleData[5].fullName}</h1>}
         <Testing login={login} error={error} />
+        </div>
       )}
     </>
   );
